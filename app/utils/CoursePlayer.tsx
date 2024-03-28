30568be52c2a707a1c521b90efaadf3bf2ece8e6
Muhammad Awais Qayyum <awaisquyyum010@gmail.com>
2024-03-28 04:12:48 -0700

    first commit

--- app/utils/CoursePlayer.tsx
+++ app/utils/CoursePlayer.tsx
@@ -14,7 +14,7 @@ const CoursePlayer: FC<Props> = ({ title, videoUrl }) => {
 
   useEffect(() => {
     axios
-      .post(`http://localhost:8000/api/v1/getVdoCipherOTP`, {
+      .post(`https://smart-study-server-s4g2.vercel.app/api/v1/getVdoCipherOTP`, {
         videoId: videoUrl,
       })
       .then((res) => {
